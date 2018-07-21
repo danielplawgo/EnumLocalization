3d4039bd3a2c9871d9dbdc097cc6f80a5a44e1ca
Daniel Plawgo <daniel@plawgo.pl>
2018-07-21 05:59:10 +0200

    Zmiana właściwej implementacji ToDisplayString na wersję najwydajniejszą - na podstawie testów z BenchmarkDotNet

--- EnumLocalization/EnumLocalization/Infrastructure/EnumExtensions.cs
+++ EnumLocalization/EnumLocalization/Infrastructure/EnumExtensions.cs
@@ -1,42 +1,53 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
+using System.Resources;
+using System.Threading;
 using System.Web;
 
 namespace System
 {
     public static class EnumExtensions
     {
-        public static string ToDisplayString(this Enum value)
+        private static IDictionary<Type, ResourceManager> _resourceManagers = new ConcurrentDictionary<Type, ResourceManager>();
+
+        public static string ToDisplayString(this Enum value, CultureInfo culture = null)
         {
-            if (value == null)
-            {
-                throw new ArgumentNullException("value");
-            }
-
             string description = value.ToString();
-            EnumDescriptionAttribute[] attributes = (EnumDescriptionAttribute[])value.GetType().GetCustomAttributes(typeof(EnumDescriptionAttribute), false);
 
-            if (attributes != null && attributes.Length > 0)
+            ResourceManager resourceManager = null;
+
+            if (_resourceManagers.TryGetValue(value.GetType(), out resourceManager) == false)
             {
-                Type resourceType = attributes[0].ResourceType;
+                EnumDescriptionAttribute[] attributes = (EnumDescriptionAttribute[])value.GetType().GetCustomAttributes(typeof(EnumDescriptionAttribute), false);
 
-                if (resourceType != null)
+                if (attributes.Any())
                 {
-                    var property = resourceType.GetProperty(description);
-                    if (property != null)
-                    {
-                        var propertyValue = property.GetValue(null);
+                    Type resourceType = attributes[0].ResourceType;
 
-                        if (propertyValue != null)
+                    if (resourceType != null)
+                    {
+                        var resourceManagerProperty = resourceType.GetProperty("ResourceManager");
+
+                        if (resourceManagerProperty != null)
                         {
-                            description = propertyValue.ToString();
+                            resourceManager = resourceManagerProperty.GetValue(null) as ResourceManager;
+
+                            _resourceManagers.Add(value.GetType(), resourceManager);
                         }
                     }
                 }
             }
+
+            if (resourceManager != null)
+            {
+                return resourceManager.GetString(description, culture ?? Thread.CurrentThread.CurrentUICulture);
+            }
+
             return description;
         }
     }
